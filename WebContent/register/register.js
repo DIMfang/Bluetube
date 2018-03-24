bace9e2a4cc228bd09dc0383eeba768832a581ad
Jean Lambert <jeeaan1702@gmail.com>
2018-03-23 22:03:47 -0700

    making a navbar with webcomponents

--- WebContent/register/register.js
+++ WebContent/register/register.js
@@ -1,7 +1,30 @@
-
+// @import {  } from "../bootstrap/";
 function $(id) {
     return document.getElementById(id);
+} 
+
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+        <nav style="background-color: #001c37" class="navbar navbar-dark border-bot-1 border-primary navbar-expand-sm|md|lg|-xl navbar-dark|light bar">
+            <a class="navbar-brand" href="">Bluetube</a>
+            <a class="navbar-brand" style="font-size:15px; color:white"> LOGIN </a>
+        </nav> `
+        this.userStatus();
+    }
+    userStatus() {
+        console.log(this.getStatus.textContent);
+        this.getStatus.textContent = 'SIGN UP';
+        this.getStatus.setAttribute('href', '#');
+    }
+    get getStatus() {
+        return this.querySelectorAll('a')[1];
+    }
 }
+customElements.define('nav-bar', NavBar);
 
 function getValues() {
     return {
@@ -15,22 +38,21 @@ function getValues() {
 }
 
 function register() {
-	let inputs = getValues();
+    let inputs = getValues();
     let configs = {
         method: 'POST',
         headers: {
-            'Content-type':'application/x-www-form-urlencoded'
+            'Content-type': 'application/x-www-form-urlencoded'
         },
         body: JSON.stringify(inputs)
     }
     fetch('../Register', configs)
-    .then(response => response.json()).then(data => {
-    	console.log(data)
-    	if(data.status == 200) {
-    		document.location.href = "../login/";
-    	}
-    }).catch(error => {
-    	console.log(error.message);
-    });    
+        .then(response => response.json()).then(data => {
+            console.log(data)
+            if (data.status == 200) {
+                document.location.href = "../login/";
+            }
+        }).catch(error => {
+            console.log(error.message);
+        });
 }
-
