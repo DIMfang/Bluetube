7a7f4fd791d4df72ec08469687574ed0712cf158
Jean Lambert <jeeaan1702@gmail.com>
2018-03-31 13:32:56 -0700

    new parent class executeSQL

---
+++ src/backend/database/ExecuteSQL.java
@@ -0,0 +1,39 @@
+package backend.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import backend.util.properties.Props;
+
+public abstract class ExecuteSQL {
+	
+	private Connection con;
+	private PreparedStatement pst;
+	
+	public ExecuteSQL() {
+		try {
+			Class.forName(Props.getDB("driver"));
+			this.con = DriverManager.getConnection(Props.getDB("url"), Props.getDB("user"), Props.getDB("password"));
+		} catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	protected ResultSet executeQuery(String query, Object... values) throws SQLException {
+		this.pst = this.con.prepareStatement(Props.getQuery(query));
+		for(int i = 0; i < values.length; i++) {
+			this.pst.setObject(i + 1, values[i]);
+		}
+		return this.pst.executeQuery();
+	}
+	protected int executeUpdate(String query, Object... values) throws SQLException {
+		this.pst = this.con.prepareStatement(Props.getQuery(query));
+		for(int i = 0; i < values.length; i++) {
+			this.pst.setObject(i+1, values[i]);
+		}
+		return this.pst.executeUpdate();
+	}
+	
+}
