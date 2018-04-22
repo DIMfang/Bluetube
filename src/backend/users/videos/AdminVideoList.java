5609494d6dd40587b9271da3573035be974be54c
Fabio Pineda <djfabiokk@gmail.com>
2018-04-21 20:40:13 -0500

    Servlet that obtains every video in site.

---
+++ src/backend/users/videos/AdminVideoList.java
@@ -0,0 +1,59 @@
+package backend.users.videos;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import backend.database.MediaQueries;
+
+/**
+ * Servlet implementation class AdminVideoList
+ */
+@WebServlet("/AdminVideoList")
+public class AdminVideoList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminVideoList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MediaQueries mq = new MediaQueries();
+		JSONArray videos = new JSONArray(); 
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		
+			try {
+				videos = mq.getAllUserVideoList();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			} finally {
+				mq.closeResources();
+			}
+		out.println(videos.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
